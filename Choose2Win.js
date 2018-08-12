cda4fbdae761bf34c084319f3b0cacad5e0ffc54
jeremyrixon <jeremy@rixon.org>
2018-08-12 19:54:25 +1000

    .

---
+++ Choose2Win.js
@@ -0,0 +1,38 @@
+﻿var Choose2Win = ludorum.Game.make({
+    name: 'Choose2Win',
+    players: ['White', 'Black'],
+
+    constructor: function Choose2Win(activePlayer, winner) {
+        ludorum.Game.call(this, activePlayer || this.players[0]);
+        this.winner = winner || null;
+    }
+});
+
+Choose2Win.prototype.result = function result() {
+    if (this.winner) {
+        return this.victory(this.winner);
+    } else {
+        return null;
+    }
+};
+
+Choose2Win.prototype.moves = function moves() {
+    if (!this.result()) {
+        var result = {};
+        result[this.activePlayer()] = ['win', 'pass'];
+        return result;
+    } else {
+        return null;
+    }
+};
+
+Choose2Win.prototype.next = function next(moves) {
+    var activePlayer = this.activePlayer(),
+        winner = moves[activePlayer] === 'win' ? activePlayer : null;
+    return new Choose2Win(this.opponent(), winner);
+};
+
+Choose2Win.prototype.toString = function toString() {
+    return 'Choose2Win('+ JSON.stringify(this.activePlayer()) +','+ JSON.stringify(this.winner) +')';
+};
+
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # pawns
 Pawns
+
+https://github.com/LeonardoVal/ludorum.js/
+
+https://github.com/LeonardoVal/ludorum.js/blob/master/docs/tutorial-game-00.md
+
